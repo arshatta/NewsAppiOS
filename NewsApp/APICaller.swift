f028bf0108c39331206c8562e646db71681997d1
Arshat Talap <t_arshat@mail.ru>
2023-06-20 14:12:48 +0600

    Created a News App Project

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,95 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by Arshat Talap on 5/12/21.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=US&apiKey=eda6154a62744b7bbad849130a7f7b6f")
+        static let searchUrlString = "https://newsapi.org/v2/everything?sortedBy=popularity&apiKey=eda6154a62744b7bbad849130a7f7b6f&q="
+    }
+
+    private init() {}
+
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+
+        task.resume()
+    }
+
+    public func search(with query: String, completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+
+        let urltring = Constants.searchUrlString + query
+        guard let url = URL(string: urltring) else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+
+        task.resume()
+    }
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
